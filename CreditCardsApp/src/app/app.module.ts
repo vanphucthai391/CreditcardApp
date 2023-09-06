c5c65414299ddb85cee3b8cbdb7b6cd4177b8d09
vanphucthai391 <43985456+vanphucthai391@users.noreply.github.com>
2023-09-06 21:20:41 +0700

    ad toastr icon

--- CreditCardsApp/src/app/app.module.ts
+++ CreditCardsApp/src/app/app.module.ts
@@ -7,7 +7,8 @@ import { PaymentDetailsComponent } from './payment-details/payment-details.compo
 import { PaymentDetailFormComponent } from './payment-details/payment-detail-form/payment-detail-form.component';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
 @NgModule({
   declarations: [
     AppComponent,
@@ -19,7 +20,9 @@ import { FormsModule } from '@angular/forms';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule, // required animations module
+    ToastrModule.forRoot(), // ToastrModule added
   ],
   providers: [],
   bootstrap: [AppComponent]
--- CreditCardsApp/src/app/payment-details/payment-detail-form/payment-detail-form.component.ts
+++ CreditCardsApp/src/app/payment-details/payment-detail-form/payment-detail-form.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { PaymentDetailService } from 'src/app/shared/payment-detail.service';
 import {NgForm} from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: 'app-payment-detail-form',
   templateUrl: './payment-detail-form.component.html',
@@ -8,8 +9,9 @@ import {NgForm} from '@angular/forms';
   ]
 })
 export class PaymentDetailFormComponent {
-  constructor(public service:PaymentDetailService){
+  constructor(public service:PaymentDetailService,private toastr: ToastrService){
   }
+  
   onsubmit(form:NgForm){
       if(this.service.formdata.paymentdetailid==0){
         this.insertrecord(form)
@@ -25,6 +27,7 @@ export class PaymentDetailFormComponent {
         console.log(res);
         this.service.resetformdetail(form);
         this.service.refreshList1();
+        this.toastr.success('Payment Details!', 'inserted successfully');
       },
       error:err=>{console.log(err)}
     })
@@ -36,6 +39,7 @@ export class PaymentDetailFormComponent {
         console.log(res);
         this.service.resetformdetail(form);
         this.service.refreshList1();//lam moi list data
+        this.toastr.info('Payment Details!', 'update successfully')
       },
       error:err=>{console.log(err)}
     })
